0bf0fe9af078867f21ecdd0d2746e316d560aa74
rOguzh <oguzhanumut@icloud.com>
2023-06-15 17:39:15 +0300

    Added precise interfaces for data types

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,13 +1,122 @@
 export interface Comic {
     id: number,
+    digitalId: number,
     title: string,
-    description: string | null,
-    thumbnail: {
-        path: string,
-        extension: string
-    }
-    creators: Array<{
-        name: string,
-        role: string
-    }>
+    issueNumber: number,
+    variantDescription: string,
+    description?: string,
+    modified: string,
+    isbn: string,
+    upc: string,
+    diamondCode: string,
+    ean: string,
+    issn: string,
+    format: string,
+    pageCount: number,
+    textObjects: TextObject[],
+    resourceURI: string,
+    urls: Url[],
+    series: Series,
+    variants: Variant[],
+    collections: Collection[],
+    collectedIssues: any[],
+    dates: Date[],
+    prices: Price[],
+    thumbnail: Thumbnail,
+    images: Image[],
+    creators: Creators,
+    characters: Characters,
+    stories: Stories,
+    events: Events,
+}
+
+export interface TextObject {
+    type: string
+    language: string
+    text: string
+}
+
+export interface Url {
+    type: string
+    url: string
+}
+
+export interface Series {
+    resourceURI: string
+    name: string
+}
+
+export interface Variant {
+    resourceURI: string
+    name: string
+}
+
+export interface Collection {
+    resourceURI: string
+    name: string
+}
+
+export interface Date {
+    type: string
+    date: string
+}
+
+export interface Price {
+    type: string
+    price: number
+}
+
+export interface Thumbnail {
+    path: string
+    extension: string
+}
+
+export interface Image {
+    path: string
+    extension: string
+}
+
+export interface Creators {
+    available: number
+    collectionURI: string
+    items: Item[]
+    returned: number
+}
+
+export interface Item {
+    resourceURI: string
+    name: string
+    role: string
+}
+
+export interface Characters {
+    available: number
+    collectionURI: string
+    items: Item2[]
+    returned: number
+}
+
+export interface Item2 {
+    resourceURI: string
+    name: string
+}
+
+export interface Stories {
+    available: number
+    collectionURI: string
+    items: Item3[]
+    returned: number
+}
+
+export interface Item3 {
+    resourceURI: string
+    name: string
+    type: string
+}
+
+export interface Events {
+    available: number
+    collectionURI: string
+    items: any[]
+    returned: number
 }

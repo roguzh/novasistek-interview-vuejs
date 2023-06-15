8ccd0c5f6f3214b228d8c1da48d9d9e7065e7f79
rOguzh <oguzhanumut@icloud.com>
2023-06-15 17:40:01 +0300

    Added toggle favourites in the header

--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -1,29 +1,42 @@
 <script setup lang="ts">
+import '@/assets/fonts/fonts.css'
+import { useStore } from 'vuex'
 
+const store = useStore()
+
+const DUMP_USER = {
+  username: 'Del Ferro'
+}
 </script>
 
 <template>
   <header class="header">
     <div class="logo">
-        <img alt="Vue logo" class="logo" src="@/assets/logo.svg"/>
+      <img alt="Vue logo" class="logo" src="@/assets/logo.svg" />
     </div>
     <div class="user-info">
-      USERNAME
+      <div id="favourite-count-wrapper" @click="store.commit('changeToggleFavourites')">
+        <span>{{ store.state.favouriteComics.length }}</span>
+        <img alt="Favourite Badge" src="src/assets/icons/filled_star.png" />
+      </div>
+      {{ DUMP_USER.username }}
     </div>
   </header>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
 .header {
   display: flex;
   align-items: center;
   justify-content: space-between;
-  padding:  0 48px;
+  padding: 0 48px;
 
   width: 100%;
   position: relative;
 
   background: var(--vt-c-black-mute);
+
+  font-family: 'Roboto';
 }
 
 .logo {
@@ -39,8 +52,50 @@
 .user-info {
   position: absolute;
   right: 48px;
+
+  font-size: 18px;
+  color: var(--vt-c-white-soft);
+
+  height: 100%;
+  display: flex;
+  align-items: center;
+
+  #favourite-count-wrapper {
+    margin: 0 10px;
+    padding: 2px 8px;
+
+    background: var(--vt-c-white-soft);
+    color: var(--vt-c-black-mute);
+
+    display: flex;
+    align-items: center;
+
+    border-radius: 8px;
+
+    line-height: 2;
+
+    cursor: pointer;
+    img {
+      height: 18px;
+      width: 18px;
+    }
+
+    span {
+      line-height: 1.7;
+      margin-top: 2px;
+      margin-right: 4px;
+    }
+  }
 }
 
-@media (min-width: 1024px) {
+@media (max-width: 927px) {
+  .logo img {
+    position: absolute;
+    left: 0;
+  }
+  .user-info {
+    position: absolute;
+    right: 16px;
+  }
 }
 </style>

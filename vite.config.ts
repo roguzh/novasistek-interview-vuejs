6e1c501901a8b4ff1b52cb127ec7815d5d4e0dc6
rOguzh <oguzhanumut@icloud.com>
2023-06-16 15:12:38 +0300

    Added type support for defineProps with setup ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,14 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import VueTypeImports from 'vite-plugin-vue-type-imports'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
+    VueTypeImports(),
   ],
   resolve: {
     alias: {
